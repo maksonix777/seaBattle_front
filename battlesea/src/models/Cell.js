85073c9901341101aace24b6855f36c37a647a65
maksonix777 <128647093+maksonix777@users.noreply.github.com>
2024-03-04 00:25:38 +0100

    Delete battlesea directory

--- battlesea/src/models/Cell.js
+++
@@ -1,9 +0,0 @@
-export class Cell {
-    constructor(board, x, y, mark) {
-        this.x = x,
-        this.y = y,
-        this.board = board,
-        this.mark = mark,
-        this.id = Math.random()
-    }
-}
